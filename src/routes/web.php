abf7cf1ede385c9dadf395b9bcfaf64c69e7525e
sadit <saditmeelek67@gmail.com>
2026-01-11 14:42:53 +0700

    web fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
-<?php
-
 use App\Http\Controllers\ImageToPdfController;
+
+Route::get('/', function () {
+    return redirect('/img2pdf');
+});
 
 Route::get('/img2pdf', [ImageToPdfController::class, 'form']);
 Route::post('/img2pdf', [ImageToPdfController::class, 'convert']);
